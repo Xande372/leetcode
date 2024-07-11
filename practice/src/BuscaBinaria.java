015dba7a96e0eeb4c3cc12f7c5f73314edfb83ae
Alexandre Direito <xandedsj@gmail.com>
2024-07-11 18:36:29 -0300

    busca_binaria

--- practice/src/BuscaBinaria.java
+++ practice/src/BuscaBinaria.java
@@ -50,4 +50,24 @@ public class BuscaBinaria {
         }
         return listaTarget;
     }
+
+    //Exercício 3: Busca Binaria.
+    public int search(int[] nums, int target) {
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
