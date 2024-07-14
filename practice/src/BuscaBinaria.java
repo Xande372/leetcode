9acb91761703a86fdf90799b248872c1f643aa34
Alexandre Direito <xandedsj@gmail.com>
2024-07-14 14:43:14 -0300

    busca binaria

--- practice/src/BuscaBinaria.java
+++ practice/src/BuscaBinaria.java
@@ -124,4 +124,32 @@ public class BuscaBinaria {
         // se o target não for achado o loop irá se encerrar quando o left for maior que o right. Neste ponto, left deverá ser o index onde o target tem que ser inserido para manter um Sorted order.
         return left;
     }
+
+    // Exercício 6: Intersection of Two Arrays II
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0,j = 0;
+
+        List<Integer> intersection = new ArrayList<>();
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else{
+                intersection.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        int[] result = new int[intersection.size()];
+        for (int k = 0; k < intersection.size(); k++) {
+            result[k] = intersection.get(k);
+        }
+        
+        return result;
+    }
 }
